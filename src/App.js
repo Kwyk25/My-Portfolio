2ada109658156c16f6b64d586756e201c6e3f102
Kyle <kylev006@gmail.com>
2023-11-07 17:53:10 -0800

    added components file, pages, and starter code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>React Personal Website</h1>{" "}
+            <h1>React Personal Website with MDBootstrap</h1>
+            <img
+  src="https://mdbcdn.b-cdn.net/img/new/standard/city/047.webp"
+  class="rounded-full"
+  alt="Townhouses and Skyscrapers"
+/>
+        </div>
+    );
 }
 
 export default App;
